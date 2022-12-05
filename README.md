a0680cdfabfe6410c8faf129049f285c15501fc3
Ubuntu <ubuntu@ip-172-31-16-200.ec2.internal>
2022-12-05 14:25:41 +0000

    added base working directory

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # LOG8430E_Review_Assignment
+
+## Directory structure
+
+1. targetDB: 
+
+- generic script file in root
+- 3 sub databases folders: mongodb, redis, orientdb
+- In each folder, we have the docker-compose file of the database as well as 6 folders for the workload (a to f). In each workload folder, we have 2 sub folders: /load: one to save the load files and /run: one to save the run output files
+
+2. ycsb-0.17.0: YCSB release file
+
+
+## script
+
+To run the script, three arguments must be passed:
+
+1. -d : refers to the database. Options are mongodb | redis | orientdb
+2. -w : workload alphabet. Options are a | b | c | d | e | f
+3. -e : execution number. Options are 1 | 2 | 3 but can be anything. It's just a number to concatenate to the output file.
+
+
+Example 1:
+
+This following command will execute the benchmark on mongodb database with the workloada and will save the result in /targetDB/mongodb/workload_a/run/output_run_a_1.txt
+
+```
+./script.sh -d mongodb -w a -e 1
+```
+
+
+Example 2:
+
+To perform 3 benchmarking processes for mongodb based on workloada, three commands need to be run one after another like so:
+
+```
+./script.sh -d mongodb -w a -e 1
+./script.sh -d mongodb -w a -e 2
+./script.sh -d mongodb -w a -e 3
+```
---
+++ targetDB/script.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+# global variables
+validDatabases=(mongodb redis orientdb)
+workloadArray=(a b c d e f)
+jmp="\n\n\n-------------------- "
+
+# ---------------------- STEP 1: VALIDATE SHELL PARAMETERS ------------------------
+printParams() {
+   echo ""
+   echo "Usage: $0 -d database -w workloadAlphabet -e executionNb"
+   echo -e "\t-d  database         : ${validDatabases[0]} | ${validDatabases[1]} | ${validDatabases[2]}"
+   echo -e "\t-w  workloadAlphabet : a | b | c | d | e | f"
+   echo -e "\t-e  executionNb      : 1 | 2 | 3"
+   exit 1 # Exit script after printing help
+}
+
+while getopts "d:w:e:" opt
+do
+   case "$opt" in
+      d ) db="$OPTARG" ;;
+      w ) workload="$OPTARG" ;;
+      e ) execution="$OPTARG" ;;
+      ? ) printParams ;; # Print parameters in case parameter is non-existent
+   esac
+done
+
+# Print parameters in case parameters are empty
+if [ -z "$db" ] || [ -z "$workload" ] || [ -z "$execution" ]
+then
+   echo "Some or all of the parameters are empty";
+   printParams
+fi
+
+# check if database is valid
+if [[ ${validDatabases[*]} =~ (^|[[:space:]])"$db"($|[[:space:]]) ]]; then
+    echo -e "Benchmarking : $db";
+else
+    echo -e "Invalid database entered";
+    printParams
+fi
+
+# check if workload is correct
+if [[ ${workloadArray[*]} =~ (^|[[:space:]])"$workload"($|[[:space:]]) ]]; then
+    echo -e "Running      : Workload $workload";
+else
+    echo -e "Invalid workload";
+    printParams
+fi
+
+echo -e "Execution    : $execution"
+
+
+# sets the YCSB arguments for individual databases
+if [ $db = ${validDatabases[0]} ]; then
+    # mongodb
+    ycsbArgs="-p mongodb.upsert=true -p mongodb.url=mongodb://mongo1:30001,mongo2:30002,mongo3:30003,mongo4:30004,mongo5:30005,mongo6:30006/?replicaSet=my-replica-set"
+elif [ $db = ${validDatabases[1]} ]; then
+    # redis
+    ycsbArgs=""
+else
+    # orientdb
+    ycsbArgs=""
+fi
+
+
+
+
+
+
+# ---------------------- STEP 2: BEGIN BENCHMARKING ------------------------
+
+cd $db
+
+# start docker
+echo -e $jmp "Starting Docker"
+docker-compose -f ./docker-compose-${db}.yml up -d
+
+# wait for nodes
+sleep_time=10
+echo -e $jmp "Waiting for ${sleep_time} seconds"
+sleep $sleep_time
+
+cd ../../ycsb-0.17.0/
+
+# load data
+echo -e $jmp "Loading data"
+./bin/ycsb load $db -s -P workloads/workload"$workload" -p recordcount=1000 $ycsbArgs > ../targetDB/$db/workload_"$workload"/load/output_load_"$workload"_"$execution".txt
+
+# execute workload
+echo -e $jmp "Executing data"
+./bin/ycsb run $db -s -P workloads/workload"$workload" -p recordcount=1000 $ycsbArgs > ../targetDB/$db/workload_"$workload"/run/output_run_"$workload"_"$execution".txt
+
+# clean
+echo -e $jmp "Cleaning"
+
+cd ../targetDB/$db/
+docker-compose -f docker-compose-${db}.yml down
+sudo rm -rf data
